998d10dd86ce7aa8c69cfb9b0138c224c4b58039
Zakeria Ali <zak.fined@gmail.com>
2020-03-27 22:39:51 +0000

    Style the video and aside

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -19,11 +19,66 @@ class Homepage extends Component {
                         </div>
                         <div className="homepage__live-channels--main__recommended">
                             <h1>Recommended live channels</h1>
-                            <div></div>
+                            <div className="homepage__live-channels--main__recommended--video">
+                                <div className="homepage__live-channels--main__recommended--video__player">
+                                    <div className="left-side">
+                                        <span>
+                                            <p>Live</p>
+                                        </span>
+                                        <span>
+                                            <p>4K</p>
+                                        </span>
+                                    </div>
+                                    <div className="bottom-side">
+                                        <div>
+                                            <i class="fas fa-play"></i>
+                                            <i class="fas fa-volume-down"></i>
+                                        </div>
+                                        <div>
+                                            <i class="fas fa-heart"></i>
+                                            <i class="fas fa-compress"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="homepage__live-channels--main__recommended--video__details">
+                                    <div className="homepage__live-channels--main__recommended--video__details--raw">
+                                        <div className="homepage__live-channels--main__recommended--video__details--raw__img"></div>
+                                        <div className="homepage__live-channels--main__recommended--video__details--raw__name">
+                                            <h2>Cayla Brister</h2>
+                                            <p>League of Legends</p>
+                                            <p>5.6K viewers</p>
+                                        </div>
+                                    </div>
+                                    <div className="homepage__live-channels--main__recommended--video__details--tags">
+                                        <span>English</span>
+                                        <span>Rank</span>
+                                        <span>Moba</span>
+                                    </div>
+                                    <div className="homepage__live-channels--main__recommended--video__details--description">
+                                        <p>League of Legends is a free-to-play competitive MOBA game with a large following in Esports</p>
+                                        <p>Tuesday-Saturday starting at anytime between 4pm-10pm pacific</p>
+                                        <p>I tend to stick to my schedule but variations of food dates, coffee, chills, IRL and hangouts</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="homepage__live-channels--aside">
-
+                        <div className="homepage__live-channels--aside__img"></div>
+                        <div className="homepage__live-channels--aside__img"></div>
+                        <div className="homepage__live-channels--aside__img larger"></div>
+                        <div className="homepage__live-channels--aside__img larger"></div>
+                        <div className="homepage__live-channels--aside__consoles">
+                            <span>
+                                <i class="fab fa-xbox"></i>
+                            </span>
+                            <span>
+                                <i class="fab fa-playstation"></i>
+                            </span>
+                            <span>
+                                <i class="fas fa-desktop"></i>
+                            </span>
+                        </div>
                     </div>
                 </div>
                 <div className="homepage__categories">
