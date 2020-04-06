07cca4b7af2750b855127ba6a9b0c26789ecbcec
Zakeria Ali <zak.fined@gmail.com>
2020-04-06 21:31:59 +0100

    Create design for form and add common classes

--- src/pages/streams/streams-create.jsx
+++ src/pages/streams/streams-create.jsx
@@ -1,16 +1,27 @@
 import React from "react"
 import {Field, reduxForm} from "redux-form"
 
-function StreamsCreate(){
-    return (
-        <div>
-            <form className="stream__form">
-                <Field name="title" />
-                <Field name="description" />
-                <button>Submit</button>
-            </form>
-        </div>
-    )
+class StreamsCreate extends React.Component {
+
+    renderInputField({input, label}){
+        return (
+            <label>{label}
+                <input {...input} className="input input-primary"/>
+            </label>
+        )
+    }
+    render(){
+        return (
+            <div className="streams__form--container">
+                <h1 className="main-page-title">Create a new stream</h1>
+                <form className="streams__form">
+                    <Field name="title" label="Title" component={this.renderInputField} />
+                    <Field name="description" label="Description" component={this.renderInputField} />
+                    <button className="btn btn-primary">Create</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default reduxForm({
