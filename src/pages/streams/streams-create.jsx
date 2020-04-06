7b136621efec60c591b9b6c41e366cd8e899304f
Zakeria Ali <zak.fined@gmail.com>
2020-04-06 20:51:12 +0100

    Create skeleton for the form

--- src/pages/streams/streams-create.jsx
+++ src/pages/streams/streams-create.jsx
@@ -1,9 +1,18 @@
 import React from "react"
+import {Field, reduxForm} from "redux-form"
 
 function StreamsCreate(){
     return (
-        <div>Streams Create</div>
+        <div>
+            <form className="stream__form">
+                <Field name="title" />
+                <Field name="description" />
+
+                <button>Submit</button>
+            </form>
+        </div>
     )
 }
+
 
 export default StreamsCreate
