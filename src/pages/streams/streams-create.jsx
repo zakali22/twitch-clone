264c6ddc7c4d51c706a9fc63c17f5b06640a0f49
Zakeria Ali <zak.fined@gmail.com>
2020-04-19 21:44:15 +0100

    Add styling for form errors

--- src/pages/streams/streams-create.jsx
+++ src/pages/streams/streams-create.jsx
@@ -3,18 +3,29 @@ import {Field, reduxForm} from "redux-form"
 
 class StreamsCreate extends React.Component {
 
-    renderInputField({input, label}){
+    renderInputField = ({input, label, meta}) => {
+        console.log(meta)
         return (
             <label>{label}
-                <input {...input} className="input input-primary"/>
+                {this.renderInputError(meta)}
+                <input {...input} className={`input input-primary ${meta.touched && meta.error ? 'input-error' : null}`}/>
             </label>
         )
     }
+
+    renderInputError({error, touched}){
+        return touched && error ? <p className="form-error">{error}</p> : null
+    }
+
+    submitHandler(formValues){
+        console.log(formValues)
+    }
+
     render(){
         return (
             <div className="streams__form--container">
                 <h1 className="main-page-title">Create a new stream</h1>
-                <form className="streams__form">
+                <form onSubmit={this.props.handleSubmit(this.submitHandler)} className="streams__form">
                     <Field name="title" label="Title" component={this.renderInputField} />
                     <Field name="description" label="Description" component={this.renderInputField} />
                     <button className="btn btn-primary">Create</button>
@@ -24,6 +35,19 @@ class StreamsCreate extends React.Component {
     }
 }
 
+const validate = (formValues) => {
+    const errors = {};
+    if(!formValues.title){
+        errors['title'] = "You must enter a title"
+    }
+    if(!formValues.description){
+        errors['description'] = "You must enter a description"
+    }
+    
+    return errors
+}
+
 export default reduxForm({
-    form: 'streamCreateForm'
+    form: 'streamCreateForm', 
+    validate
 })(StreamsCreate)
